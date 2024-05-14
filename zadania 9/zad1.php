e6963906cedb9a01b42358183f07bad443c205e7
Klemensmandzaro <58343390+Klemensmandzaro@users.noreply.github.com>
2024-05-14 16:27:20 +0200

    Add files via upload

---
+++ zadania 9/zad1.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form name="formularz1" method="GET">
+        <input type="date" name="pierwsza">
+        <button type="submit">Działaj</button>
+    </form>
+    <?php
+        if (isset($_GET['pierwsza']))
+        {
+            if (isset($_GET['pierwsza']))
+        {
+            $a=$_GET['pierwsza'];
+            echo date('l', strtotime($a));
+            echo("<br>");
+            $data1=date('Y');
+            $data2=date('F');
+            $data3=date('d');
+
+            if ($data2==date('F', strtotime($a)) && $data3>=date('d', strtotime($a)))
+            {
+                echo (date('Y')-date('Y', strtotime($a)));
+            }
+            else if ($data2==date('F', strtotime($a)) && $data3<date('d', strtotime($a)))
+            {
+                echo (date('Y')-date('Y', strtotime($a))-1);
+            }
+            else if ($data2<date('F', strtotime($a)))
+            {
+                echo (date('Y')-date('Y', strtotime($a))-1);
+            }
+            else
+            {
+                echo (date('Y')-date('Y', strtotime($a)));
+            }
+            $data4=date('z');
+            echo("<br>");
+            if ($data4>=date('z', strtotime($a)))
+            {
+                echo ($data4-date('z', strtotime($a)));
+            }
+            else
+            {
+                $dzien=($data4-date('z', strtotime($a)));
+                $dzien+=365;//bo nastepny rok jest przestepny a ten to juz mija
+                echo $dzien;
+            }
+
+        }
+
+            
+        }
+    ?>
+</body>
+</html>
