83332f3cd0b9e8ab842684236bd0a582d8703f01
Klemensmandzaro <58343390+Klemensmandzaro@users.noreply.github.com>
2024-05-28 15:50:54 +0200

    Add files via upload

---
+++ zadania11/zad1.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        class NoweAuto{
+            public $name="";
+            public $cena=0;
+            public $kurs=0;
+            function __construct($name, $cena, $kurs) {
+                $this->name = $name;
+                $this->age = $age;
+                $this->kurs = $kurs;
+            }
+
+            function ObliczCene(){
+                return $this->cena
+            }
+    
+        }
+    ?>
+</body>
+</html>
